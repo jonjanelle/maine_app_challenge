ec418f0bf920b631b08fed72659320fda5300967
Jon Janelle <jon@nextgengolf.org>
2018-12-17 20:31:20 -0500

    change api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:4200', 'app-challenge-resources.herokuapp.com']
+    origins ['localhost:4200', 'maine-app-challenge.herokuapp.com']
 
     resource '*',
       headers: :any,
